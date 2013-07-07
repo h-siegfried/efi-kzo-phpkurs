0fe9360574ba1f6021a781efc64d6e08f570f4e5
hanspi <comp@si-gi.ch>
2013-07-07 14:54:41 +0200

    Endlich Ordnung in den Commits von .metadata

--- PHP-Einstieg/pages/index.php
+++ PHP-Einstieg/pages/index.php
@@ -1,16 +1,53 @@
 <?php
 
-
-
 ?>
-
+ 
+ 
 <!DOCTYPE html>
-<html>
+<html lang="de">
+
+<!-- 
+	Nun folgt der "head"-Bereich.
+	"head" ist nicht etwa der Seitenkopf, 
+	der head-Bereich enthaelt vielmehr Informationen, die dem 
+	Browser anzeigen, wie er die Webseite aufbereiten soll 
+-->
 <head>
-<meta charset="utf-8">
-<title>Insert title here</title>
+<!-- 
+	Die Tags im Head-Bereich werden spaeter in diesem
+	Kurs erklaert. Insbesondere werden Sie dann verstehen,
+	was "charset" bedeutet 
+-->
+<meta charset="UTF-8">
+<title>Meine erste PHP-Webseite</title>
 </head>
+
+
+<!-- 
+	Es folgt der Body-Bereich der Webseite.
+	Der Body-Bereich enthaelt alles, was auf der 
+	Webseite sichtbar ist. 
+-->
 <body>
 
-</body>
+<!-- 
+	Hier folgt ein <p>-Tag. <p> bedeutet "paragraph",
+	auf deutsch also ein Absatz. 
+-->
+<p>
+Im n&auml;chsten Absatz erscheint die Ausgabe meines PHP-Programms:
+</p>
+
+<p>
+<?php 
+// Auf der folgenden Zeile können Sie 
+// die Ausgabe programmieren.
+
+?>
+</p>
+
+
+
+</body> <!-- Ende des Body-Bereichs -->
+
 </html>
