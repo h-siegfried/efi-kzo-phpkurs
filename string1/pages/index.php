9d5e4714e05c43ee5df70dafbcd455e2052b8163
Hanspeter Siegfried <comp@si-gi.ch>
2013-07-22 15:44:27 +0200

    Projekt string1 angepasst: HTML-Seite vorbereitet, damit TN nur noch PHP-Code schreiben muessen.

--- string1/pages/index.php
+++ string1/pages/index.php
@@ -1,16 +1,30 @@
 <?php
-$string1 = "";
-$string2 = "";
+
+
+$vorstellung = "";
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 <meta charset="utf-8">
-<title>Insert title here</title>
+<title>String-&Uuml;bungen</title>
 </head>
+
+
 <body>
+<h1>&Uuml;bungen mit dem Datentyp String</h1>
+
+<p>Im n&auml;chsten Absatz erscheint die Ausgabe meines PHP-Programms:</p>
+
+<p>
+<?php 
+
+?>
+</p>
 
 </body>
+
 </html>
