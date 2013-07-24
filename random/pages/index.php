e2080653841fcef22f66b3c46f7bc8fb494abddc
Hanspeter Siegfried <comp@si-gi.ch>
2013-07-24 10:17:21 +0200

    Projekt random ergänzt.

---
+++ random/pages/index.php
@@ -0,0 +1,29 @@
+<?php
+$zufallszahl;
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>String-&Uuml;bungen</title>
+<link rel="stylesheet" href="../style/styles_string2.css" />
+</head>
+
+
+<body>
+<h1>&Uuml;bung: String extrahieren</h1>
+
+<p>Im n&auml;chsten Absatz erscheint die Ausgabe meiner Zufallszahl.</p>
+
+<p>
+<?php 
+
+?>
+</p>
+
+</body>
+
+</html>
