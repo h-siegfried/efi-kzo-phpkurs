fcf98d0eca6c2dac66ff275f7e411060d5b511ed
Hanspeter Siegfried <comp@si-gi.ch>
2013-07-24 09:16:48 +0200

    Aufgabe zur Funktion substr vorbereitet.

---
+++ string2/pages/index.php
@@ -0,0 +1,31 @@
+<?php
+$vollname = "Barack Obama";
+$start; 
+$nachname;
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>String-&Uuml;bungen</title>
+</head>
+
+
+<body>
+<h1>&Uuml;bung: String extrahieren</h1>
+
+<p>Im n&auml;chsten Absatz erscheint die Ausgabe meines PHP-Programms:<br />
+Der Nachname.</p>
+
+<p>
+<?php 
+print($nachname);
+?>
+</p>
+
+</body>
+
+</html>
