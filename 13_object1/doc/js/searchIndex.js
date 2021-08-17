6385e06fc5d7333c936e5aad0b585fbf583c5602
hanspi <comp@si-gi.ch>
2021-08-17 15:40:18 +0200

    Teil Objekt bis vor PDO fertig. Doc neu erzeugt.

---
+++ 13_object1/doc/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Begruesser",
+            "name": "Begruesser",
+            "summary": "Class\u0020Begruesser",
+            "url": "classes/Begruesser.html"
+        },                {
+            "fqsen": "\\Begruesser\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor",
+            "url": "classes/Begruesser.html#method___construct"
+        },                {
+            "fqsen": "\\Begruesser\u003A\u003AgetBegruessung\u0028\u0029",
+            "name": "getBegruessung",
+            "summary": "getBegruessung\u003A\u0020Returns\u0020a\u0020string\u0020containing\u0020the\u0020begruessung\u0020for\u0020your\u0020web\u0020page.",
+            "url": "classes/Begruesser.html#method_getBegruessung"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
